82e9db555da55190ae223e7b0ee477d1ec6f9ce2
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-17 00:05:24 +0900

    feat: Setup Main 정렬 시작점 메인 메서드와 테스트 구현

---
+++ oop-practice-FastCampus/src/test/java/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main() {
+        // Given
+        String[] args = {"3", "4", "2", "1"};
+
+        // When & Then
+        Main.main(args);
+    }
+}
